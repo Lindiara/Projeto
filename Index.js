2daa702adcf2bab1de6b0d46ee1cc6ce1b0591af
Lindy <lindy.ag13@gmail.com>
2026-02-26 14:32:47 -0300

    first commit

---
+++ Index.js
@@ -0,0 +1,8 @@
+alert("Fala, dev!")
+alert(10 * 100 + "abraços!")
+let boasVindas = "Fala, Dev!"
+alert(boasVindas)
+boasVindas = "Fala, Dev! Beleza?!"
+alert(boasVindas)
+
+console.log("Hello world")
